f9c09473a4035ecb0e03bfa98b87fc2388409cdb
Ed <edwardydlee17375@gmail.com>
2021-09-14 02:05:30 +0800

    add variables

---
+++ Day1/Variables/variables.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//宣告變數 a,b
+	var a int = 10
+	b := 15
+	fmt.Println(a, b)
+
+	//宣告多個變數
+	var c int = 110
+	c, d := 100, 120
+	fmt.Println(c, d)
+
+	//宣告字串
+	var e = "你好,歡迎!"
+	fmt.Println(e)
+
+	//宣告數值
+	var f int = 10
+	fmt.Println(f)
+
+	//宣告布林
+	var g = true
+	fmt.Println(g)
+
+	//宣告浮點數
+	var h float32 = 3.141592
+	fmt.Println(h)
+
+	//指標型
+	var x string = "a"
+	var pf *string = &x
+	fmt.Println(pf)
+	fmt.Println(*pf)
+
+}
